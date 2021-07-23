d55e5e9bfe098887660ea65435a7f910d83039d9
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-07-23 11:22:15 -0600

    commit tree df65a6978c3d73954d4ad1bccea15063ff3dfe5d

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,27 @@
 class SongsController < ApplicationController
   def index
-    @songs = Song.all
+    if params[:artist_id]
+      @artist = Artist.find_by_id(params[:artist_id])  #assigns the artist based on the artist_id passed in through songs params
+      if !@artist.nil?
+        @songs = @artist.songs  #assigns @songs for the songs that belong_to that artist.
+      else
+        redirect_to artists_path, notice: "Artist not found"
+      end
+    else
+      @songs = Song.all
+    end
   end
 
   def show
+    if params[:artist_id]
+      @artist = Artist.find_by_id(params[:artist_id])
+      @song = @artist.songs.find_by_id(params[:id])   
+      if @song.nil? 
+        redirect_to artist_songs_path(@artist), alert: "Song not found"
+      end
+    else 
     @song = Song.find(params[:id])
+    end
   end
 
   def new
